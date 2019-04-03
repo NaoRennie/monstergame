6df354065004c8053333d8c3b5865a5babed295d
NaomiRennie <naomi.lily.405@gmail.com>
2019-04-03 13:10:50 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="ui segment">
+      <div class="ui two column very relaxed grid">
+        <div class="column">
+          <p>you</p>
+          <p class="bar"></p>
+        </div>
+        <div class="column">
+          <p>monster</p>
+          <p class="bar"></p>
+        </div>
+      </div>
+      <div class="ui vertical divider">vs</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+.bar {
+  display: inline-block;
+  background-color: pink;
+  width: 100px;
+  height: 10px;
+}
+</style>
